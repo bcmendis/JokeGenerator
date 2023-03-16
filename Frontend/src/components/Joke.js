3afda0db0755b8d46f7821e0260af8d7037ac180
bcmendis <benclintonmendis@gmail.com>
2023-03-16 11:43:01 -0400

    Revert "initial commit"
    This reverts commit 88a9e0c1188fece35b33ca396915c0dfd8dfcdce.

--- Frontend/src/components/Joke.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Joke.module.css";
-
-const Joke = (props) => {
-  return <div className={classes.joke}>{props.joke}</div>;
-};
-
-export default Joke;
