7bd36644d911b380fcd33e96be316ab70412d8fc
bcmendis <benclintonmendis@gmail.com>
2023-03-16 14:49:41 -0400

    cors config 5

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
@@ -12,26 +12,30 @@ const openai = new OpenAIApi(configuration);
 
 // Set up the server
 const app = express();
-//app.use(cors());
+app.use(
+  cors({
+    origin: "https://joke-gpt-bcmendis.vercel.app",
+  })
+);
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//     return;
+//   }
+//   next();
+// });
 
 // Set up the ChatGPT endpoint
 app.post("/joke", async (req, res) => {
