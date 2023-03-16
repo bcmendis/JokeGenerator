be09d2cfd763fda95a1de6ac06d1a46690611fc7
bcmendis <benclintonmendis@gmail.com>
2023-03-16 14:43:02 -0400

    cors config 4

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
@@ -12,7 +12,7 @@ const openai = new OpenAIApi(configuration);
 
 // Set up the server
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
