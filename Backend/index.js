d81b26f0c2600bfd93b2aa672831a8a65e56334a
bcmendis <benclintonmendis@gmail.com>
2023-03-16 14:10:14 -0400

    cors config

--- Backend/index.js
+++ Backend/index.js
@@ -15,6 +15,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  next();
+});
+
 // Set up the ChatGPT endpoint
 app.post("/joke", async (req, res) => {
   // Get the prompt from the request
